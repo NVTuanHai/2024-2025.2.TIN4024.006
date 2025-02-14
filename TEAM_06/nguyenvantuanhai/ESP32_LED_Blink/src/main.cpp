f638350508c68e49c0ab19419c7603a353543eab
NVTuanHai <21t1020341@husc.edu.vn>
2025-02-14 16:07:47 +0700

    NVTuanHai

--- TEAM_06/nguyenvantuanhai/ESP32_LED_Blink/src/main.cpp
+++ TEAM_06/nguyenvantuanhai/ESP32_LED_Blink/src/main.cpp
@@ -1,17 +1,47 @@
 #include <Arduino.h>
 
-int ledPin = 5; // chân gpio 2 (thường nối sẵn led trên board)
+#define GREEN_LED 16  // Chân kết nối đèn xanh
+#define YELLOW_LED 17 // Chân kết nối đèn vàng
+#define RED_LED 5     // Chân kết nối đèn đỏ
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(ledPin, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(YELLOW_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  Serial.begin(115200); // Khởi động Serial Monitor
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH); // Bật LED
-  Serial.println("LED -> ON");
-  delay(1000);
-  digitalWrite(ledPin, LOW); //Tắt LED
-  Serial.println("LED -> OFF");
-  delay(1000);
+  // Đèn xanh sáng trong 10 giây
+  digitalWrite(GREEN_LED, HIGH);
+  digitalWrite(YELLOW_LED, LOW);
+  digitalWrite(RED_LED, LOW);
+  for (int i = 10; i > 0; i--) {
+    Serial.print("Đèn xanh: ");
+    Serial.print(i);
+    Serial.println(" giây");
+    delay(1000);
+  }
+
+  // Đèn vàng sáng trong 3 giây
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(YELLOW_LED, HIGH);
+  digitalWrite(RED_LED, LOW);
+  for (int i = 3; i > 0; i--) {
+    Serial.print("Đèn vàng: ");
+    Serial.print(i);
+    Serial.println(" giây");
+    delay(1000);
+  }
+
+  // Đèn đỏ sáng trong 7 giây
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(YELLOW_LED, LOW);
+  digitalWrite(RED_LED, HIGH);
+  for (int i = 7; i > 0; i--) {
+    Serial.print("Đèn đỏ: ");
+    Serial.print(i);
+    Serial.println(" giây");
+    delay(1000);
+  }
 }
